e284a801d44b5f53736ac67fa68dd1eddc0f47e6
lhkhiem28 <lhkhiem28@gmail.com>
2023-04-16 14:31:41 +0000

    initial

---
+++ source/models/models.py
@@ -0,0 +1,36 @@
+import os, sys
+from libs import *
+
+class PretextsCA(nn.Module):
+    def __init__(self, 
+        num_classes = 0, 
+    ):
+        super(PretextsCA, self).__init__()
+        self.resnet50_simclr = self.load_pretext("/home/ubuntu/khiem.lh/Free/Continual-Learning/pretexts/resnet50_simclr")
+        self.resnet50_mocov2 = self.load_pretext("/home/ubuntu/khiem.lh/Free/Continual-Learning/pretexts/resnet50_mocov2")
+        self.vits16_dino = torch.hub.load(
+            "facebookresearch/dino:main", "dino_vits16", 
+            map_location = "cpu", 
+        )
+
+    def load_pretext(self, 
+        state_dict_path, 
+    ):
+        state_dict = torch.load(
+            state_dict_path, 
+            map_location = "cpu", 
+        )
+        state_dict = state_dict["classy_state_dict"]["base_model"]["model"]["trunk"]
+        state_dict = collections.OrderedDict([
+            (key.replace("_feature_blocks.", ""), value) if "_feature_blocks." in key else (key, value) 
+            for key, value in state_dict.items()
+        ])
+
+        pretext = torchvision.models.__dict__["resnet50"]()
+        pretext.fc = nn.Identity()
+        pretext.load_state_dict(
+            state_dict = state_dict, 
+            strict = True, 
+        )
+
+        return pretext
