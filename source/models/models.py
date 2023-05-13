2c8ee18e062faaff22566d80f5a8b4afcdd36757
lhkhiem28 <lhkhiem28@gmail.com>
2023-05-13 08:01:13 +0000

    initial

--- source/models/models.py
+++ source/models/models.py
@@ -6,14 +6,16 @@ class PretextsCA(nn.Module):
         num_classes = 0, 
     ):
         super(PretextsCA, self).__init__()
-        self.pretext_resnet50_simclr = self.load_pretext_vissl("/home/ubuntu/khiem.lh/Free/Continual-Learning/pretexts/resnet50_simclr")
-        self.pretext_resnet50_mocov2 = self.load_pretext_vissl("/home/ubuntu/khiem.lh/Free/Continual-Learning/pretexts/resnet50_mocov2")
+        self.pretext_resnet50_simclr = self.load_pretext("resnet50_simclr")
+        self.pretext_resnet50_mocov2 = self.load_pretext("resnet50_mocov2")
         self.mha_resnet50_simclr = nn.MultiheadAttention(
-            embed_dim = 300, num_heads = 2, 
+            embed_dim = 300, 
+            num_heads = 2, dropout = 0.2, 
             batch_first = True, 
         )
         self.mha_resnet50_mocov2 = nn.MultiheadAttention(
-            embed_dim = 300, num_heads = 2, 
+            embed_dim = 300, 
+            num_heads = 2, dropout = 0.2, 
             batch_first = True, 
         )
 
@@ -21,28 +23,36 @@ class PretextsCA(nn.Module):
             300, num_classes, 
         )
 
-    def load_pretext_vissl(self, 
+    def load_pretext(self, 
         state_dict_path, 
     ):
-        state_dict = torch.load(
-            state_dict_path, 
-            map_location = "cpu", 
-        )
-        state_dict = state_dict["classy_state_dict"]["base_model"]["model"]["trunk"]
-        state_dict = collections.OrderedDict([
-            (key.replace("_feature_blocks.", ""), value) if "_feature_blocks." in key else (key, value) 
-            for key, value in state_dict.items()
-        ])
-        pretext = torchvision.models.__dict__["resnet50"]()
-        pretext.fc = nn.Identity()
-        pretext.load_state_dict(
-            state_dict = state_dict, 
-            strict = True, 
-        )
+        if "dino" not in state_dict_path:
+            state_dict = torch.load(
+                "../../pretexts/" + state_dict_path, map_location = "cpu", 
+            )["classy_state_dict"]["base_model"]["model"]["trunk"]
+            state_dict = collections.OrderedDict([
+                (key.replace("_feature_blocks.", ""), value) if "_feature_blocks." in key else (key, value) for key, value in state_dict.items()
+            ])
 
-        pretext.fc = nn.Linear(
-            2048, 300, 
-        )
+            pretext = torchvision.models.__dict__["resnet50"](); pretext.fc = nn.Identity(); 
+            pretext.load_state_dict(
+                state_dict = state_dict, strict = True, 
+            )
+            pretext.fc = nn.Linear(
+                2048, 300, 
+            )
+        else:
+            if "dinov2" in state_dict_path:
+                pretext = torch.hub.load(
+                    "facebookresearch/dinov2", state_dict_path, 
+                )
+            else:
+                pretext = torch.hub.load(
+                    "facebookresearch/dino:main", state_dict_path, 
+                )
+            pretext.head = nn.Linear(
+                384, 300, 
+            )
 
         return pretext
 
